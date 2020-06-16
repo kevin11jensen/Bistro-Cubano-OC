ca509cb7b59aec3d6010d261bc29bc7c5bb5ed06
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-06-16 09:25:42 -0700

    variables set up, destyle css installed, meyers reset added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <MainContainer>
+
+      </MainContainer>
     </div>
   );
 }
 
 export default App;
+
+const MainContainer = styled.div `
+  background-color: var(--bg);
+  height: 100vh;
+`
