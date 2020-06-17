0e2866195adaaa53f8ff7e98dde0d78035941f7c
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-06-16 19:53:15 -0700

    testing overflow hidden on html for mobile view

--- src/components/About.js
+++ src/components/About.js
@@ -19,6 +19,7 @@ export default function AboutUs() {
 const About = styled.section `
     display: flex;
     background-color: var(--black);
+    
     img {
         width: 80%;
         background-color: var(--black);
